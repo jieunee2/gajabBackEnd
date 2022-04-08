dfa58e7fb720f7902af279800757005b325bea0f
jieunee2 <980lje@naver.com>
2022-04-09 01:42:38 +0900

    [#36] 커뮤니티 내 게시물 댓글 수정 및 삭제 기능 구현

--- src/main/java/com/gajob/service/posts/CommentsServiceImpl.java
+++ src/main/java/com/gajob/service/posts/CommentsServiceImpl.java
@@ -2,6 +2,7 @@ package com.gajob.service.posts;
 
 import com.gajob.dto.posts.CommentsDto;
 import com.gajob.dto.posts.CommentsResponseDto;
+import com.gajob.entity.posts.Comments;
 import com.gajob.entity.posts.Posts;
 import com.gajob.entity.user.User;
 import com.gajob.repository.posts.CommentsRepository;
@@ -26,10 +27,35 @@ public class CommentsServiceImpl implements CommentsService {
     User user = userRepository.findOneWithAuthoritiesByEmail(
         SecurityUtil.getCurrentUsername().get()).get();
     Posts posts = postsRepository.findById(id)
-        .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 존재하지 않습니다." + id));
+        .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 존재하지 않습니다."));
 
     return new CommentsResponseDto(commentsRepository.save(commentsDto.toEntity(user, posts)));
   }
 
+  // 댓글 수정
+  @Transactional
+  public CommentsResponseDto update(Long postId, Long commentsId, CommentsDto commentsDto) {
+    Posts posts = postsRepository.findById(postId)
+        .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 존재하지 않습니다." + postId));
+
+    Comments comments = commentsRepository.findById(commentsId)
+        .orElseThrow(() -> new IllegalArgumentException("해당 댓글이 존재하지 않습니다. " + commentsId));
+
+    comments.update(commentsDto.getComment());
+
+    CommentsResponseDto commentsResponseDto = new CommentsResponseDto(comments);
+
+    return commentsResponseDto;
+  }
+
+  // 댓글 삭제
+  @Transactional
+  public String delete(Long id) {
+    Comments comments = commentsRepository.findById(id)
+        .orElseThrow(() -> new IllegalArgumentException("해당 댓글이 존재하지 않습니다."));
+    commentsRepository.delete(comments);
+
+    return "comments-delete";
+  }
 
 }
