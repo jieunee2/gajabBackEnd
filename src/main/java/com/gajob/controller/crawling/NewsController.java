c0a6dc284f2d0e7ce36d90a36d6180f66f632ff3
jieunee2 <980lje@naver.com>
2022-04-09 21:30:02 +0900

    [#49] Controller 어노테이션 수정

--- src/main/java/com/gajob/controller/crawling/NewsController.java
+++ src/main/java/com/gajob/controller/crawling/NewsController.java
@@ -4,19 +4,16 @@ import com.gajob.dto.crawling.NewsDto;
 import com.gajob.entity.crawling.News;
 import com.gajob.service.crawling.NewsCrawling;
 import lombok.RequiredArgsConstructor;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
-
 import org.springframework.web.bind.annotation.CrossOrigin;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@RequiredArgsConstructor
+@RestController
 @CrossOrigin(origins = "http://localhost:3000/")
 @RequestMapping("/issue")
 public class NewsController {
@@ -25,8 +22,7 @@ public class NewsController {
 
   @GetMapping("/news") // 크롤링한 뉴스 데이터 가져오기
   public ResponseEntity<NewsDto> getNews() {
-    return new ResponseEntity(newsCrawling.getNews(),
-        HttpStatus.OK);
+    return new ResponseEntity(newsCrawling.getNews(), HttpStatus.OK);
   }
 
   @PostMapping("/news") // 크롤링한 뉴스 데이터 DB 저장
--- src/main/java/com/gajob/controller/posts/PostsController.java
+++ src/main/java/com/gajob/controller/posts/PostsController.java
@@ -11,9 +11,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,12 +29,6 @@ public class PostsController {
 
   @GetMapping("/posts/{id}") // 게시물 조회(조회시 조회수 증가)
   public ResponseEntity updateView(@PathVariable Long id) {
-
-//    ResponseEntity.ok(postsService.updateView(id));
-//    return "posts-read";
-    return ResponseEntity.ok(postsService.updateView(id));
-  }
-
     return ResponseEntity.ok(postsService.updateView(id));
   }
 
