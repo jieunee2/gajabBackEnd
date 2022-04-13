f22b20e95f3a13c0917e06fd3f9469241e2960e3
jieunee2 <980lje@naver.com>
2022-04-13 19:39:24 +0900

    [#65] REST API 디자인 기반으로 URL 수정

--- src/main/java/com/gajob/controller/posts/PostsController.java
+++ src/main/java/com/gajob/controller/posts/PostsController.java
@@ -13,10 +13,12 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/community")
 @CrossOrigin(origins = "http://localhost:3000/")
 public class PostsController {
 
