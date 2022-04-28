6c80369e0dd0b55d21b2cca348b7cd9b8f21d45c
jieunee2 <980lje@naver.com>
2022-04-28 18:28:30 +0900

    [#118] 댓글 목록 별도로 조회

--- src/main/java/com/gajob/service/study/StudyCommentsService.java
+++ src/main/java/com/gajob/service/study/StudyCommentsService.java
@@ -2,10 +2,13 @@ package com.gajob.service.study;
 
 import com.gajob.dto.study.StudyCommentsDto;
 import com.gajob.dto.study.StudyCommentsResponseDto;
+import java.util.List;
 
 public interface StudyCommentsService {
 
   StudyCommentsResponseDto save(Long postId, StudyCommentsDto studyCommentsDto); //댓글 저장
+
+  List<StudyCommentsResponseDto> getStudyComments(Long postId); //게시물 별로 댓글 조회
 
   StudyCommentsResponseDto update(Long postId, Long commentId,
       StudyCommentsDto studyCommentsDto); //댓글 수정
