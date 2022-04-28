16c6818163b9f390500d542267750613b1cfbe1c
jieunee2 <980lje@naver.com>
2022-04-28 18:17:48 +0900

    [#118] 댓글 목록 별도로 조회

--- src/main/java/com/gajob/service/posts/PostsCommentsService.java
+++ src/main/java/com/gajob/service/posts/PostsCommentsService.java
@@ -2,10 +2,13 @@ package com.gajob.service.posts;
 
 import com.gajob.dto.posts.PostsCommentsDto;
 import com.gajob.dto.posts.PostsCommentsResponseDto;
+import java.util.List;
 
 public interface PostsCommentsService {
 
   PostsCommentsResponseDto save(Long postId, PostsCommentsDto postsCommentsDto); //댓글 저장
+
+  List<PostsCommentsResponseDto> getPostsComments(Long postId); //게시물 별로 댓글 조회
 
   PostsCommentsResponseDto update(Long postId, Long commentId,
       PostsCommentsDto postsCommentsDto); //댓글 수정
