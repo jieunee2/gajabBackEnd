1c4f93ad63e0621c4b1553e12b15946d75fe5b7d
jieunee2 <980lje@naver.com>
2022-05-15 15:42:42 +0900

    [#166] 회원가입 시 교내 이메일 인증 구현

--- src/main/java/com/gajob/controller/mail/MailController.java
+++ src/main/java/com/gajob/controller/mail/MailController.java
@@ -1,9 +1,11 @@
 package com.gajob.controller.mail;
 
 import com.gajob.dto.mail.MailDto;
+import com.gajob.service.mail.EmailVerificationMailService;
+import com.gajob.service.mail.EmailVerificationMailServiceImpl;
 import com.gajob.service.mail.FindPasswordMailService;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,9 +17,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class MailController {
 
   private final FindPasswordMailService findPasswordMailService;
+  private final EmailVerificationMailService emailVerificationMailService;
 
   // 임시 비밀번호를 담은 메일 전송
-  @Transactional
   @PostMapping("/find-password")
   public String sendEmail(@RequestBody MailDto mailDto) {
     findPasswordMailService.createMailAndUpdatePassword(mailDto.getEmail());
@@ -25,5 +27,25 @@ public class MailController {
 
     return "send-mail-successful";
   }
+
+  // 교내 이메일 인증 코드 전송
+  @PostMapping("/email")
+  public String emailAuth(@RequestBody MailDto mailDto) throws Exception {
+    emailVerificationMailService.createMessage(mailDto);
+    emailVerificationMailService.sendSimpleMessage(mailDto);
+
+    return "send-mail-successful";
+  }
+
+  //  // 교내 이메일 인증 코드 검증
+  @PostMapping("/verifyCode")
+  public String verifyCode(@RequestBody Map<String, String> code) {
+    if (EmailVerificationMailServiceImpl.num.equals(code.get("code"))) {
+      return "authentication-success";
+    } else {
+      return "authentication-failed";
+    }
+  }
+
 
 }
