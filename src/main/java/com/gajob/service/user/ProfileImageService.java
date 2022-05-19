823b95a5aa9bd8d416fece5e5e98985029f33677
jieunee2 <980lje@naver.com>
2022-05-19 18:09:33 +0900

    [#161] 유저 프로필 이미지 업로드

---
+++ src/main/java/com/gajob/service/user/ProfileImageService.java
@@ -0,0 +1,9 @@
+package com.gajob.service.user;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface ProfileImageService {
+
+  void upload(MultipartFile multipartFile); // 프로필 사진 업로드
+
+}
