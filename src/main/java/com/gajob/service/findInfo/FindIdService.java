68054b460aa5bf385863460dff459e689cc9a3b2
jieunee2 <980lje@naver.com>
2022-05-26 17:29:27 +0900

    [#215] 아이디 찾기 Response Type 변경

--- src/main/java/com/gajob/service/findInfo/FindIdService.java
+++ src/main/java/com/gajob/service/findInfo/FindIdService.java
@@ -1,9 +1,11 @@
 package com.gajob.service.findInfo;
 
 import com.gajob.dto.user.UserDto;
+import com.gajob.repository.user.UserMapping;
+import java.util.List;
 
 public interface FindIdService {
 
-  String findId(UserDto userDto); // 아이디 찾기
+  List<UserMapping> findId(UserDto userDto); // 아이디 찾기
 
 }
