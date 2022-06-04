6ae287d56197ccc38c070efa11ef257f869065b6
DoeunLee <ehdms00@naver.com>
2022-06-05 03:32:48 +0900

    [#271] 채용정보 스크랩 정보 추가

--- src/main/java/com/gajob/dto/openApi/JobScrapResponseDto.java
+++ src/main/java/com/gajob/dto/openApi/JobScrapResponseDto.java
@@ -8,12 +8,24 @@ public class JobScrapResponseDto {
 
     private Long id;
     private String title;
+    private String company;
+    private String salaryType;
+    private String salary;
+    private String closeDate;
+    private String career;
+    private String region;
     private String scrapDate;
     private String wantedInfoUrl;
 
     public JobScrapResponseDto(JobScrap jobScrap) {
         this.id = jobScrap.getId();
         this.title = jobScrap.getJobFrame().getTitle();
+        this.company = jobScrap.getJobFrame().getCompany();
+        this.salaryType = jobScrap.getJobFrame().getSalaryType();
+        this.salary = jobScrap.getJobFrame().getSalary();
+        this.closeDate = jobScrap.getJobFrame().getCloseDate();
+        this.career = jobScrap.getJobFrame().getCareer();
+        this.region = jobScrap.getJobFrame().getRegion();
         this.scrapDate = jobScrap.getCreatedDate();
         this.wantedInfoUrl = jobScrap.getJobFrame().getWantedInfoUrl();
     }
